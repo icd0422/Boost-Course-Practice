5dbb9b80e6ea20d9289d52b7468d7598fe89f342
ggujunhee <icd16244@gmail.com>
2019-07-30 13:15:42 +0900

    2-3-2 Show Notification Dialog Box

--- app/src/main/java/com/example/boostcoursepractice/MainActivity.java
+++ app/src/main/java/com/example/boostcoursepractice/MainActivity.java
@@ -1,67 +1,60 @@
 package com.example.boostcoursepractice;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btn ;
-    Button btn2 ;
-    Button btn3 ;
+    Button btn;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = (Button) findViewById(R.id.button);
+        btn = (Button)findViewById(R.id.button);
+        text = (TextView)findViewById(R.id.textView);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast toast = Toast.makeText(getApplicationContext(), "위치가 바뀐 토스트 메세지", Toast.LENGTH_LONG) ;
-                toast.setGravity(Gravity.TOP | Gravity.LEFT, 200, 200);
-                toast.show();
+                showMessage();
             }
         });
+    }
 
-        btn2 = (Button) findViewById(R.id.button2);
+    public void showMessage() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("안내");
+        builder.setMessage("종료하시겠습니까?");
+        builder.setIcon(android.R.drawable.ic_dialog_alert);
 
-        btn2.setOnClickListener(new View.OnClickListener() {
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                LayoutInflater layoutInflater = getLayoutInflater();
-                View layout = layoutInflater.inflate(R.layout.toastborder, (ViewGroup)findViewById(R.id.toast_layout_root));
-
-                TextView text = (TextView) layout.findViewById(R.id.tv);
-                text.setText("모양을 바꾼 토스트입니다.");
-
-                Toast toast = new Toast(getApplicationContext());
-                toast.setGravity(Gravity.CENTER, 0, -100);
-                toast.setDuration(Toast.LENGTH_LONG);
-                toast.setView(layout);
-
-                toast.show();
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Snackbar.make(text, "예 버튼이 눌렸습니다.", Snackbar.LENGTH_LONG).show();
             }
         });
 
-        btn3 = (Button) findViewById(R.id.button3);
-
-        btn3.setOnClickListener(new View.OnClickListener() {
+        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "스낵바 입니다.", Snackbar.LENGTH_LONG).show();
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Snackbar.make(text, "아니오 버튼이 눌렸습니다.", Snackbar.LENGTH_LONG).show();
             }
         });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
