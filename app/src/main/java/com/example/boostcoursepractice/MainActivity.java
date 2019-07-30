27c97e1ed4cbd37103ddbc2d99c0ad1fe492239b
ggujunhee <icd16244@gmail.com>
2019-07-30 09:39:10 +0900

    2-3-1 Show Toast

--- app/src/main/java/com/example/boostcoursepractice/MainActivity.java
+++ app/src/main/java/com/example/boostcoursepractice/MainActivity.java
@@ -3,12 +3,65 @@ package com.example.boostcoursepractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn ;
+    Button btn2 ;
+    Button btn3 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "위치가 바뀐 토스트 메세지", Toast.LENGTH_LONG) ;
+                toast.setGravity(Gravity.TOP | Gravity.LEFT, 200, 200);
+                toast.show();
+            }
+        });
+
+        btn2 = (Button) findViewById(R.id.button2);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater layoutInflater = getLayoutInflater();
+                View layout = layoutInflater.inflate(R.layout.toastborder, (ViewGroup)findViewById(R.id.toast_layout_root));
+
+                TextView text = (TextView) layout.findViewById(R.id.tv);
+                text.setText("모양을 바꾼 토스트입니다.");
+
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.CENTER, 0, -100);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(layout);
+
+                toast.show();
+            }
+        });
+
+        btn3 = (Button) findViewById(R.id.button3);
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "스낵바 입니다.", Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 }
