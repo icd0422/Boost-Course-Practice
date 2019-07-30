3e5a0f423eb468fba1a6f12602122535d45b2d18
ggujunhee <icd16244@gmail.com>
2019-07-30 20:29:42 +0900

    2-4-1 NinePatch Image

--- app/src/main/java/com/example/boostcoursepractice/MainActivity.java
+++ app/src/main/java/com/example/boostcoursepractice/MainActivity.java
@@ -15,46 +15,12 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
-    TextView text;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = (Button)findViewById(R.id.button);
-        text = (TextView)findViewById(R.id.textView);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showMessage();
-            }
-        });
     }
 
-    public void showMessage() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("안내");
-        builder.setMessage("종료하시겠습니까?");
-        builder.setIcon(android.R.drawable.ic_dialog_alert);
 
-        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Snackbar.make(text, "예 버튼이 눌렸습니다.", Snackbar.LENGTH_LONG).show();
-            }
-        });
-
-        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Snackbar.make(text, "아니오 버튼이 눌렸습니다.", Snackbar.LENGTH_LONG).show();
-            }
-        });
-
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-    }
 }
