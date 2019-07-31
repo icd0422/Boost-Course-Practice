fa00c23302716d6c442e3019e970c621593b53b7
ggujunhee <icd16244@gmail.com>
2019-07-31 09:37:08 +0900

    2-5-1 Understanding Inflation

--- app/src/main/java/com/example/boostcoursepractice/MainActivity.java
+++ app/src/main/java/com/example/boostcoursepractice/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.boostcoursepractice;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -15,12 +18,25 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    FrameLayout container ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        container = (FrameLayout) findViewById(R.id.container);
 
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+                layoutInflater.inflate(R.layout.sum1, container, true);
+
+            }
+        });
+    }
 
 }
