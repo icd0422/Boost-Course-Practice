2837aefcc0fc1427f368537e6123f2454522d0b5
ggujunhee <icd16244@gmail.com>
2019-07-31 11:12:50 +0900

    2-6-2 Using Spinner

--- app/src/main/java/com/example/boostcoursepractice/MainActivity.java
+++ app/src/main/java/com/example/boostcoursepractice/MainActivity.java
@@ -8,8 +8,11 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -18,25 +21,35 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    FrameLayout container ;
+    String[] items2 = new String[5];
+    String[] items = {"소녀시대", "걸스데이", "티아라", "여자친구"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        container = (FrameLayout) findViewById(R.id.container);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        final TextView textView = (TextView) findViewById(R.id.textView);
 
-        Button btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                textView.setText(items[i]);
+            }
 
-                layoutInflater.inflate(R.layout.sum1, container, true);
-
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                textView.setText("선택안됨");
             }
         });
+
     }
 
 }
