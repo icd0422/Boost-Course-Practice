76dcafb0e023a10ccd8aeb3f10e0126a1a86f2f4
ggujunhee <icd16244@gmail.com>
2019-08-02 15:59:55 +0900

    3-4-1 Service

--- app/src/main/java/com/example/boostcoursepractice/MainActivity.java
+++ app/src/main/java/com/example/boostcoursepractice/MainActivity.java
@@ -1,43 +1,22 @@
 package com.example.boostcoursepractice;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import org.w3c.dom.Text;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button closeBtn ;
+    EditText editText;
+    Button serviceStartBtn ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,70 +24,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "OnCreate() 호출됨", Toast.LENGTH_LONG).show();
+        serviceStartBtn = (Button) findViewById(R.id.btn_start_service);
+        editText = (EditText) findViewById(R.id.editText);
 
-        closeBtn = (Button) findViewById(R.id.btn_close);
-        closeBtn.setOnClickListener(new View.OnClickListener() {
+        serviceStartBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                String name = editText.getText().toString();
+
+                Intent intent = new Intent(getApplicationContext(), MyService.class);
+                intent.putExtra("command", "show");
+                intent.putExtra("name", name) ;
+                startService(intent);
+
             }
         });
+
+        Intent intent = getIntent() ;
+        processCommand(intent) ;
     }
 
     @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        processCommand(intent) ;
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this, " onStart() 호출됨", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this, "onStop() 호출됨", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "onDestroy() 호출됨", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this, "onPause() 호출됨", Toast.LENGTH_LONG).show();
-
-        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
-        if(pref == null) Toast.makeText(this, "pref가 없습니다.", Toast.LENGTH_LONG).show();
-        SharedPreferences.Editor editor = pref.edit();
-        editor.putString("name", "소녀시대");
-        editor.commit() ;
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this, "onResume() 호출됨", Toast.LENGTH_LONG).show();
-
-        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
-        if(pref == null) Toast.makeText(this, "pref가 없습니다.", Toast.LENGTH_LONG).show();
-        if(pref != null)
+    private void processCommand(Intent intent)
+    {
+        if(intent != null)
         {
-            String name = pref.getString("name", "");
-            Toast.makeText(this, "복구된 데이터 : " + name, Toast.LENGTH_LONG).show();
+            String command = intent.getStringExtra("command") ;
+            String name = intent.getStringExtra("name");
+            Toast.makeText(getApplicationContext(), "서비스로부터 전달 받은 데이터 : " + command + ", " + name, Toast.LENGTH_LONG).show();
+
         }
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-
-        Toast.makeText(this, "onRestart() 호출됨", Toast.LENGTH_LONG).show();
     }
 }
