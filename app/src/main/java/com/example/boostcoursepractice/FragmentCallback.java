28fe9ff9056c057955ea5073b1e048a6b5b1851a
ggujunhee <icd16244@gmail.com>
2019-08-15 18:50:25 +0900

    5-1-1 Use Thread

--- app/src/main/java/com/example/boostcoursepractice/FragmentCallback.java
+++
@@ -1,9 +0,0 @@
-package com.example.boostcoursepractice;
-
-import android.os.Bundle;
-
-public interface FragmentCallback {
-
-    public void onFragmentSelected(int position, Bundle bundle) ;
-
-}
--- app/src/main/java/com/example/boostcoursepractice/MainActivity.java
+++ app/src/main/java/com/example/boostcoursepractice/MainActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 
@@ -21,115 +25,137 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.Menu;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, FragmentCallback {
+public class MainActivity extends AppCompatActivity {
 
-    Fragment fragment1 ;
-    Fragment fragment2;
-    Fragment fragment3 ;
-    Toolbar toolbar ;
+    TextView textView;
+    ValueHandler valueHandler;
+
+    int value = 0;
+    boolean running = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
+        valueHandler = new ValueHandler();
 
-        fragment1 = new Fragment1();
-        fragment2 = new Fragment2();
-        fragment3 = new Fragment3() ;
+        final Handler handler2 = new Handler();
 
-        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment1).commit();
+        textView = (TextView) findViewById(R.id.textView);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //new BackgroundThread().start();
+
+                new Thread(new Runnable() {
+
+                    int threadValue = 0;
+                    //int value = 0;
+
+                    @Override
+                    public void run() {
+                        running = true;
+                        while (running) {
+                            //value += 1;
+                            threadValue += 1;
+                            handler2.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    value+=1;
+                                    textView.setText("현재 값" + value);
+                                }
+                            });
+
+                            try {
+                                Thread.sleep(1);
+                            } catch (Exception e) {
+                            }
+                        }
+
+                        Log.d("JJH","threadValue 값 : " + threadValue);
+                        //Log.d("JJH", "value 값 : " + value);
+                    }
+                }).start();
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new BackgroundThread().start();
+            }
+        });
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                running = false;
+            }
+        });
+
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //textView.setText("현재 값 : " + value);
+            }
+        });
     }
 
-    @Override
-    public void onFragmentSelected(int position, Bundle bundle) {
-        Fragment curFragment = null ;
+    class BackgroundThread extends Thread {
 
-        if(position == 0)
-        {
-            curFragment = fragment1 ;
-            toolbar.setTitle("첫번째 화면");
+        int threadValue = 0;
+        //int value = 0;
+
+        public void run() {
+            running = true;
+            while (running) {
+                //value += 1;
+                threadValue += 1;
+                Message message = valueHandler.obtainMessage();
+                //Bundle bundle = new Bundle();
+                //bundle.putInt("value", value);
+                //message.setData(bundle);
+                valueHandler.sendMessage(message);
+                try {
+                    Thread.sleep(1);
+                } catch (Exception e) {
+                }
+            }
+
+            Log.d("JJH","threadValue 값 : " + threadValue );
+            //Log.d("JJH", "value 값 : " + value);
         }
 
-        else if(position == 1)
-        {
-            curFragment = fragment2 ;
-            toolbar.setTitle("두번째 화면");
-        }
 
-        else if(position == 2)
-        {
-            curFragment = fragment3 ;
-            toolbar.setTitle("세번째 화면");
-        }
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, curFragment).commit();
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+    class ValueHandler extends Handler {
+
+        int sum = 0;
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+
+            value+=1 ;
+            //Bundle bundle = msg.getData();
+            //int value = bundle.getInt("value");
+
+            textView.setText("현재 값 : " + value);
+            //textView.setText("메세지큐의 마지막 값 : " + value);
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_0) {
-            Toast.makeText(this, "첫번째 화면 선택됨", Toast.LENGTH_SHORT).show();
-            onFragmentSelected(0, null);
-        } else if (id == R.id.nav_1) {
-            Toast.makeText(this, "두번째 화면 선택됨", Toast.LENGTH_SHORT).show();
-            onFragmentSelected(1, null);
-        } else if (id == R.id.nav_2) {
-            Toast.makeText(this, "세번째 화면 선택됨", Toast.LENGTH_SHORT).show();
-            onFragmentSelected(2, null);
-        }
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
 
