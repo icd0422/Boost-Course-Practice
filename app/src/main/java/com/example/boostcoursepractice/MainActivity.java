2c3238f356a9643afef318541418fc31bb1d5c37
ggujunhee <icd16244@gmail.com>
2019-08-17 21:20:34 +0900

    5-6-1 Download Image

--- app/src/main/java/com/example/boostcoursepractice/MainActivity.java
+++ app/src/main/java/com/example/boostcoursepractice/MainActivity.java
@@ -1,132 +1,43 @@
 package com.example.boostcoursepractice;
 
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
 import android.view.View;
 
-import androidx.annotation.NonNull;
-import androidx.core.view.GravityCompat;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-
-import android.view.MenuItem;
-
-import com.google.android.material.navigation.NavigationView;
-import com.google.gson.Gson;
-
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
 
-import android.view.Menu;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.HttpURLConnection;
-import java.net.Socket;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    ImageView imageView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.textView);
-        Button button = (Button) findViewById(R.id.button);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        button = (Button) findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendRequest();
+                sendImageRequest();
             }
         });
-
-        if(AppHelper.requestQueue == null) AppHelper.requestQueue = Volley.newRequestQueue(getApplicationContext());
-
     }
 
-    public void sendRequest()
-    {
-        //String url = "http://www.google.co.kr";
-        String url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=430156241533f1d058c603178cc3ca0e&targetDt=20120101";
-        StringRequest request = new StringRequest(
-                Request.Method.GET,
-                url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        //println("응답 -> " + response);
+    public void sendImageRequest() {
+        String url = "https://lh5.googleusercontent.com/p/AF1QipP88UCg_d5npMcxmBkm_ZXqmxm2korTx5Bdya9k=w237-h160-k-no";
+        ImageLoadTask task = new ImageLoadTask(url, imageView);
+        task.execute();
 
-                        processResponse(response) ;
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        println("에러 -> " + error.getMessage());
-                    }
-                }
-        ){
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {////Post방식으로 파라미터를 전달해주고싶을 때
-                Map<String, String> params = new HashMap<String, String>() ;
-
-                return params ;
-            }
-        };
-
-        request.setShouldCache(false);
-        AppHelper.requestQueue.add(request);
-        println("응답 보냄");
     }
-
-    public void processResponse(String response)
-    {
-        Gson gson = new Gson();
-        MovieList movieList = gson.fromJson(response, MovieList.class) ;
-
-        if(movieList != null)
-        {
-            int cnt = movieList.boxOfficeResult.dailyBoxOfficeList.size();
-            for(int i=0; i<cnt ; i++)
-            {
-                println(movieList.boxOfficeResult.dailyBoxOfficeList.get(i).movieNm);
-            }
-        }
-    }
-
-
-    public void println(String data) {
-        textView.append(data + "\n");
-    }
-
 }
