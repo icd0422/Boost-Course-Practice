d2620c9c512feee8187f2aeaf2c3f5c5202878ec
ggujunhee <icd16244@gmail.com>
2019-08-25 17:29:40 +0900

    6-5-1 Check your internet connection

--- app/src/main/java/com/example/boostcoursepractice/MainActivity.java
+++ app/src/main/java/com/example/boostcoursepractice/MainActivity.java
@@ -24,179 +24,33 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText;
-    EditText editText2;
-    EditText editText3;
-    EditText editText4;
-    EditText editText5;
-    TextView textView;
-    SQLiteDatabase database;
+    TextView textView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = (EditText) findViewById(R.id.editText);
-        editText2 = (EditText) findViewById(R.id.editText2);
-        editText3 = (EditText) findViewById(R.id.editText3);
-        editText4 = (EditText) findViewById(R.id.editText4);
-        editText5 = (EditText) findViewById(R.id.editText5);
-
-        textView = (TextView) findViewById(R.id.textView);
+        final TextView textView= (TextView) findViewById(R.id.textView);
 
         Button button = (Button) findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String databaseName = editText.getText().toString();
-                openDatabase(databaseName);
-            }
-        });
+                int status = NetworkStatus.getConnectivityStatus(getApplicationContext());
 
-        Button button2 = (Button) findViewById(R.id.button2);
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String tableName = editText2.getText().toString();
-                createTable(tableName);
-            }
-        });
-
-        Button button3 = (Button) findViewById(R.id.button3);
-
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String name = editText3.getText().toString().trim();
-                String ageStr = editText4.getText().toString().trim();
-                String mobile = editText5.getText().toString().trim();
-
-                int age = -1;
-                try {
-                    age = Integer.parseInt(ageStr);
-                } catch (Exception e) {
-                    e.printStackTrace();
+                if(status == NetworkStatus.TYPE_MOBILE)
+                {
+                    textView.setText("모바일로 연결됨");
+                }else if(status == NetworkStatus.TYPE_WIFI)
+                {
+                    textView.setText("무선랜으로 연결됨");
+                }else{
+                    textView.setText("연결 안됨");
                 }
-
-                insertData(name, age, mobile);
-
             }
         });
 
-        Button button4 = (Button) findViewById(R.id.button4);
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String tableName = editText2.getText().toString();
-                selectData(tableName);
-            }
-        });
     }
-
-    public void openDatabase(String databaseName) {
-        println("openDatabase() 호출됨");
-
-        /*
-        database = openOrCreateDatabase(databaseName, MODE_PRIVATE, null);
-
-        if (database != null) {
-            println("데이터 베이스 오픈됨");
-        }
-        */
-
-        DatabaseHelper helper = new DatabaseHelper(this, databaseName, null, 3 );
-        database = helper.getWritableDatabase();
-
-    }
-
-    public void createTable(String tableName) {
-        println("crateTable() 호출됨");
-
-        if (database != null) {
-            String sql = "create table if not exists " + tableName + "(_id integer PRIMARY KEY autoincrement , name text, age integer, mobile text) ";
-            database.execSQL(sql);
-
-            println("테이블 생성됨.");
-        } else {
-            println("먼저 데이터베이스를 오픈하세요");
-        }
-    }
-
-    public void insertData(String name, int age, String mobile) {
-        println("insertData() 호출됨");
-
-        if (database != null) {
-            String sql = "insert into customer(name, age, mobile) values(?,?,?)";
-            Object[] params = {name, age, mobile};
-
-            database.execSQL(sql, params);
-            println("데이터 추가됨");
-        } else {
-            println("먼저 데이터베이스를 오픈하세요.");
-        }
-    }
-
-    public void selectData(String tableName) {
-        println("selectData() 호출됨");
-
-        if (database != null) {
-            String sql = "select name, age, mobile from " + tableName;
-            Cursor cursor = database.rawQuery(sql, null);
-            println("조회된 데이터 개수 : " + cursor.getCount());
-
-            for (int i = 0; i < cursor.getCount(); i++) {
-                cursor.moveToNext();
-                String name = cursor.getString(0);
-                int age = cursor.getInt(1);
-                String mobile = cursor.getString(2);
-
-                println("#" + i + "->" + name + ", " + age + ", " + mobile);
-            }
-
-            cursor.close();
-        } else {
-            println("먼저 데이터베이스를 오픈하세요.");
-        }
-    }
-
-
-    public void println(String s) {
-        textView.append(s + "\n");
-    }
-
-    class DatabaseHelper extends SQLiteOpenHelper {
-
-        public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-            super(context, name, factory, version);
-        }
-
-        @Override
-        public void onCreate(SQLiteDatabase sqLiteDatabase) {
-            println("onCreate() 호출됨");
-
-            String tableName = "customer";
-
-            String sql = "create table if not exists " + tableName + "(_id integer PRIMARY KEY autoincrement , name text, age integer, mobile text) ";
-            sqLiteDatabase.execSQL(sql);
-
-            println("테이블 생성됨.");
-
-        }
-
-        @Override
-        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-            println("onUpgrade() 호출됨" + i + ", " + i1) ;
-
-            if(i1 > 1)
-            {
-                String tableName = "customer" ;
-                sqLiteDatabase.execSQL("drop table if exists " + tableName);
-            }
-        }
-    }
-
-
 }
